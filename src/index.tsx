fdd355fe81e3cdd286b8f0122cffe94f2a858b6d
Abner Almeida <85902242+abneralmeidacord@users.noreply.github.com>
2024-11-20 00:13:15 -0300

    Revert "Fixes"
    This reverts commit 420f392802426ba1554fdbd27217c63c5e285f74.

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import Logo from "./assets/images/Logo.png";
 import "./App.css";
 
-export const App = () => {
+const App = () => {
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <div className="App">
